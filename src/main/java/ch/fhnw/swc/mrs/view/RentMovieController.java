dd3f1ddd376c02f47975bc0fd213b2ae23fbb9fa
Max Erb <max.erb@students.fhnw.ch>
2017-11-03 08:40:19 +0100

    fixed typo in RentMovieController that lead to the build failing (was alert instead of Alert) and fixed the missing imports

--- src/main/java/ch/fhnw/swc/mrs/view/RentMovieController.java
+++ src/main/java/ch/fhnw/swc/mrs/view/RentMovieController.java
@@ -10,14 +10,17 @@ import ch.fhnw.swc.mrs.model.User;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 
-/** ava FX controller class for rent movies tab. */
+/** Java FX controller class for rent movies tab. */
 public class RentMovieController implements MRSServiceUtilizer {
   @FXML
   private TableView<Movie> availableMoviesTable;
